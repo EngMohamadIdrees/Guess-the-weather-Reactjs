8ff3d9906601735c83763c448c12567a73ef2066
Mohamad Idrees <106817730+EngMohamadIdrees@users.noreply.github.com>
2023-03-02 18:14:58 +0200

    change location titile

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ class App extends React.Component {
     if (!this.state.errorMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <Spinner message="Please accept location  request." />;
+    return <Spinner message="Please accept locationnnnnnnn  request." />;
   }
 
   render() {
